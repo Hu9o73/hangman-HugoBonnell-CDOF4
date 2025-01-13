2f3f1f14410ede614328f3a6e3a4047b75190add
Hu9o73 <hbonnell@live.fr>
2025-01-13 18:36:20 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+ name='{Hangman}',
+ version='0.1',
+ author='{Hugo BONNELL}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
